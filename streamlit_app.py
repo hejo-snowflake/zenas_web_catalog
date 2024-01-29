0b0770e3e5294d61a754ba955a42dee699a30d54
hejo-snowflake <157604264+hejo-snowflake@users.noreply.github.com>
2024-01-29 13:54:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,13 @@ streamlit.title("Zena's Amazing Athleisure Catalog")
 # Snowflake-related functions
 def get_snowflake_data():
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("SELECT * FROM zenas_athleisure_db.products.catalog_for_website")
+        my_cur.execute("SELECT * FROM catalog_for_website")
         return my_cur.fetchall() # all lines
 
 # get data
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_catalog = pandas.DataFrame(get_snowflake_data())
+my_cnx.close()
 
 # add widget for sweatshirt selection
 sweater_selected = streamlit.selectbox("Pick an item", my_catalog[0].values.tolist())
