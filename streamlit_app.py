042535bd3bbf30eec532064740f6f37d9531120c
hejo-snowflake <157604264+hejo-snowflake@users.noreply.github.com>
2024-01-29 13:32:43 +0100

    Create streamlit_app.py
    Imports and Title

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+# import and set up streamlit app
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+streamlit.title('Zena's Amazing Athleisure Catalog')
+
+
+
